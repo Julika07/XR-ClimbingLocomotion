e07b42f6310041cefa14828a35f00fc60d9be235
Julika <132730416+Julika07@users.noreply.github.com>
2023-08-19 13:08:30 +0530

    Climbing Procedure

--- Assets/Climbing/Scripts/VelocityContainer.cs
+++ Assets/Climbing/Scripts/VelocityContainer.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 /// <summary>
 /// Instead of creating a custom controller, we add a simple component to hold the action.
 /// </summary>
 public class VelocityContainer : MonoBehaviour
 {
+    [SerializeField] private InputActionProperty velocityInput;
+
+    public Vector3 Velocity => velocityInput.action.ReadValue<Vector3>();   
 
 }
